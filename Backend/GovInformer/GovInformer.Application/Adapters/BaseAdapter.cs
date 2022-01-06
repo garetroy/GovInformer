8b4eede0d4f8d9878ed8fbf97d52aa64f0ebf667
garetroy <mygarett@gmail.com>
2022-01-05 17:57:27 -0800

    In-Memory caching to reduce calls to the DB

--- Backend/GovInformer/GovInformer.Application/Adapters/BaseAdapter.cs
+++ Backend/GovInformer/GovInformer.Application/Adapters/BaseAdapter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Runtime.Caching;
 using System.Threading.Tasks;
 
 namespace GovInformer.Application.Adapters
@@ -9,12 +10,25 @@ namespace GovInformer.Application.Adapters
     {
         protected async Task<T> Get<T>(string url, Func<Stream, Task<T>> processFunc)
         {
+            if (MemoryCache.Default[url] is T result)
+            {
+                return result;
+            }
+
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
 
             using HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
             using Stream stream = response.GetResponseStream();
-            return await processFunc(stream);
+
+            result = await processFunc(stream);
+
+            var policy = new CacheItemPolicy() { AbsoluteExpiration = DateTime.Now.AddMinutes(MinutesToLive) };
+            MemoryCache.Default.Set(url, result, policy);
+
+            return result;
         }
+
+        private readonly int MinutesToLive = 10;
     }
 }
--- Backend/GovInformer/GovInformer.Application/Congress/CongressGatherer.cs
+++ Backend/GovInformer/GovInformer.Application/Congress/CongressGatherer.cs
@@ -7,6 +7,7 @@ using GovInformer.Models.Congress.Senators;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Threading.Tasks;
 
 namespace GovInformer.Application.Congress
@@ -15,147 +16,161 @@ namespace GovInformer.Application.Congress
     {
         public async Task<HashSet<Senator>> GatherAllSenators()
         {
-            var result = await new TheUnitedStatesIOAdapter().GetAllCurrentLegislators();
+            return await Gather(System.Reflection.MethodBase.GetCurrentMethod().Name, async () =>
+            {
+                var legislators = await new TheUnitedStatesIOAdapter().GetAllCurrentLegislators();
 
-            var sentors = result.Select(leg =>
-                new Senator
-                {
-                    GovTrackID = leg.IDs.GovTrackID.ToString(),
-                    BioGuideID = leg.IDs.BioGuideID.ToString(),
-                    FirstName = leg.Name.FirstName,
-                    LastName = leg.Name.LastName,
-                    Birthday = leg.Bio.Birthday,
-                    PhoneNumber = leg.Terms.Last().PhoneNumber,
-                    Website = leg.Terms.Last().SenatorSite,
-                    CongressType = CongressType.Parse(leg.Terms.Last().CongressType),
-                    PoliticalParty = PoliticalParty.Parse(leg.Terms.Last().PoliticalParty),
-                    StateTerritory = StateTerritory.Parse(leg.Terms.Last().StateTerritory),
-                    SenateClass = SenateClass.Parse(leg.Terms.Last().SenatorClass),
-                    TermStart = leg.Terms.Last().TermStart,
-                    TermEnd = leg.Terms.Last().TermEnd,
-                    YearsOfSenatorExperience = leg.Terms.Where(term => CongressType.Parse(leg.Terms.Last().CongressType) == CongressType.Senator).Select(term => (int)((term.TermEnd - term.TermStart).TotalDays / 365.2425)).ToList().Sum(),
-                }
-            ).Where(leg => leg.CongressType == CongressType.Senator).ToDictionary(leg => leg.BioGuideID);
+                var sentors = legislators.Select(leg =>
+                    new Senator
+                    {
+                        GovTrackID = leg.IDs.GovTrackID.ToString(),
+                        BioGuideID = leg.IDs.BioGuideID.ToString(),
+                        FirstName = leg.Name.FirstName,
+                        LastName = leg.Name.LastName,
+                        Birthday = leg.Bio.Birthday,
+                        PhoneNumber = leg.Terms.Last().PhoneNumber,
+                        Website = leg.Terms.Last().SenatorSite,
+                        CongressType = CongressType.Parse(leg.Terms.Last().CongressType),
+                        PoliticalParty = PoliticalParty.Parse(leg.Terms.Last().PoliticalParty),
+                        StateTerritory = StateTerritory.Parse(leg.Terms.Last().StateTerritory),
+                        SenateClass = SenateClass.Parse(leg.Terms.Last().SenatorClass),
+                        TermStart = leg.Terms.Last().TermStart,
+                        TermEnd = leg.Terms.Last().TermEnd,
+                        YearsOfSenatorExperience = leg.Terms.Where(term => CongressType.Parse(leg.Terms.Last().CongressType) == CongressType.Senator).Select(term => (int)((term.TermEnd - term.TermStart).TotalDays / 365.2425)).ToList().Sum(),
+                    }
+                ).Where(leg => leg.CongressType == CongressType.Senator).ToDictionary(leg => leg.BioGuideID);
 
-            await GatherCommitteeMembershipForLegislators(sentors);
-
-            return sentors.Values.ToHashSet();
+                await GatherCommitteeMembershipForLegislators(sentors);
+                return sentors.Values.ToHashSet();
+            });
         }
 
         public async Task<HashSet<Representative>> GatherAllRepresentatives()
         {
-            var result = await new TheUnitedStatesIOAdapter().GetAllCurrentLegislators();
+            return await Gather(System.Reflection.MethodBase.GetCurrentMethod().Name, async () =>
+            {
+                var legislators = await new TheUnitedStatesIOAdapter().GetAllCurrentLegislators();
 
-            var representatives = result.Select(leg =>
-                new Representative
-                {
-                    GovTrackID = leg.IDs.GovTrackID.ToString(),
-                    BioGuideID = leg.IDs.BioGuideID.ToString(),
-                    FirstName = leg.Name.FirstName,
-                    LastName = leg.Name.LastName,
-                    Birthday = leg.Bio.Birthday,
-                    PhoneNumber = leg.Terms.Last().PhoneNumber,
-                    Website = leg.Terms.Last().SenatorSite,
-                    CongressType = CongressType.Parse(leg.Terms.Last().CongressType),
-                    PoliticalParty = PoliticalParty.Parse(leg.Terms.Last().PoliticalParty),
-                    StateTerritory = StateTerritory.Parse(leg.Terms.Last().StateTerritory),
-                    District = leg.Terms.Last().District,
-                    TermStart = leg.Terms.Last().TermStart,
-                    TermEnd = leg.Terms.Last().TermEnd,
-                    YearsOfRepresentativeExperience = leg.Terms.Where(term => CongressType.Parse(leg.Terms.Last().CongressType) == CongressType.Representative).Select(term => (int)((term.TermEnd - term.TermStart).TotalDays / 365.2425)).ToList().Sum(),
-                }
-            ).Where(leg => leg.CongressType == CongressType.Representative).ToDictionary(leg => leg.BioGuideID);
+                var representatives = legislators.Select(leg =>
+                    new Representative
+                    {
+                        GovTrackID = leg.IDs.GovTrackID.ToString(),
+                        BioGuideID = leg.IDs.BioGuideID.ToString(),
+                        FirstName = leg.Name.FirstName,
+                        LastName = leg.Name.LastName,
+                        Birthday = leg.Bio.Birthday,
+                        PhoneNumber = leg.Terms.Last().PhoneNumber,
+                        Website = leg.Terms.Last().SenatorSite,
+                        CongressType = CongressType.Parse(leg.Terms.Last().CongressType),
+                        PoliticalParty = PoliticalParty.Parse(leg.Terms.Last().PoliticalParty),
+                        StateTerritory = StateTerritory.Parse(leg.Terms.Last().StateTerritory),
+                        District = leg.Terms.Last().District,
+                        TermStart = leg.Terms.Last().TermStart,
+                        TermEnd = leg.Terms.Last().TermEnd,
+                        YearsOfRepresentativeExperience = leg.Terms.Where(term => CongressType.Parse(leg.Terms.Last().CongressType) == CongressType.Representative).Select(term => (int)((term.TermEnd - term.TermStart).TotalDays / 365.2425)).ToList().Sum(),
+                    }
+                ).Where(leg => leg.CongressType == CongressType.Representative).ToDictionary(leg => leg.BioGuideID);
 
-            await GatherCommitteeMembershipForLegislators(representatives);
+                await GatherCommitteeMembershipForLegislators(representatives);
 
-            return representatives.Values.ToHashSet();
+                return representatives.Values.ToHashSet();
+            });
         }
 
-        public async Task<List<Committee>> GatherAllCommittees()
+        public async Task<HashSet<Committee>> GatherAllCommittees()
         {
-            var result = await new TheUnitedStatesIOAdapter().GetAllCurrentCommittees();
+            return await Gather(System.Reflection.MethodBase.GetCurrentMethod().Name, async () =>
+            {
+                var committees = await new TheUnitedStatesIOAdapter().GetAllCurrentCommittees();
 
-            return
-                result.Select(comm =>
-                    new Committee
-                    {
-                        ID = comm.ID,
-                        CongressType = CongressType.Parse(comm.CongressType),
-                        Name = comm.Name,
-                        Website = comm.Website,
-                        MinorityWebsite = comm.MinorityWebsite,
-                        Jurisdiction = comm.Jurisdiction,
-                        PhoneNumber = comm.PhoneNumber,
-                        Address = comm.Address,
-                        SubCommittees = comm.SubCommittees?.Select(scomm => new SubCommittees
+                return
+                    committees.Select(comm =>
+                        new Committee
                         {
-                            ID = comm.ID + scomm.ID,
-                            Address = scomm.Address,
-                            Phone = scomm.PhoneNumber,
-                            Name = scomm.Name,
-                        }).ToHashSet()
-                    }
-                ).ToList();
+                            ID = comm.ID,
+                            CongressType = CongressType.Parse(comm.CongressType),
+                            Name = comm.Name,
+                            Website = comm.Website,
+                            MinorityWebsite = comm.MinorityWebsite,
+                            Jurisdiction = comm.Jurisdiction,
+                            PhoneNumber = comm.PhoneNumber,
+                            Address = comm.Address,
+                            SubCommittees = comm.SubCommittees?.Select(scomm => new SubCommittees
+                            {
+                                ID = comm.ID + scomm.ID,
+                                Address = scomm.Address,
+                                Phone = scomm.PhoneNumber,
+                                Name = scomm.Name,
+                            }).ToHashSet()
+                        }
+                    ).ToHashSet();
+            });
         }
 
         public async Task<Dictionary<string, HashSet<CommitteeMembership>>> GatherAllCommitteeMembership()
         {
-            var result = await new TheUnitedStatesIOAdapter().GetAllCurrentCommitteeMembership();
-
-            var ret = new Dictionary<string, HashSet<CommitteeMembership>>();
-            foreach (var kvp in result)
+            return await Gather(System.Reflection.MethodBase.GetCurrentMethod().Name, async () =>
             {
-                var committeeId = kvp.Key;
-                var set = new HashSet<CommitteeMembership>();
-                if (kvp.Value == null)
-                {
-                    ret.Add(committeeId, set);
-                    continue;
-                }
+                var result = await new TheUnitedStatesIOAdapter().GetAllCurrentCommitteeMembership();
 
-                foreach (var membership in kvp.Value)
+                var ret = new Dictionary<string, HashSet<CommitteeMembership>>();
+                foreach (var kvp in result)
                 {
-                    set.Add(new CommitteeMembership
+                    var committeeId = kvp.Key;
+                    var set = new HashSet<CommitteeMembership>();
+                    if (kvp.Value == null)
                     {
-                        BioGuideID = membership.BioGuideID,
-                        CommitteeRank = membership.CommitteeRank,
-                        Majority = membership.MajorityOrMinority.ToLower() == "majority",
-                        MemberName = membership.MemberName
-                    });
+                        ret.Add(committeeId, set);
+                        continue;
+                    }
+
+                    foreach (var membership in kvp.Value)
+                    {
+                        set.Add(new CommitteeMembership
+                        {
+                            BioGuideID = membership.BioGuideID,
+                            CommitteeRank = membership.CommitteeRank,
+                            Majority = membership.MajorityOrMinority.ToLower() == "majority",
+                            MemberName = membership.MemberName
+                        });
+                    }
+
+                    ret.Add(committeeId, set);
                 }
 
-                ret.Add(committeeId, set);
-            }
-
-            return ret;
+                return ret;
+            });
         }
 
 
         private async Task<Dictionary<string, Committee>> GatherAllCommitteesDict()
         {
-            var result = await new TheUnitedStatesIOAdapter().GetAllCurrentCommittees();
+            return await Gather(System.Reflection.MethodBase.GetCurrentMethod().Name, async () =>
+            {
+                var result = await new TheUnitedStatesIOAdapter().GetAllCurrentCommittees();
 
-            return
-                result.Select(comm =>
-                    new Committee
-                    {
-                        ID = comm.ID,
-                        CongressType = CongressType.Parse(comm.CongressType),
-                        Name = comm.Name,
-                        Website = comm.Website,
-                        MinorityWebsite = comm.MinorityWebsite,
-                        Jurisdiction = comm.Jurisdiction,
-                        PhoneNumber = comm.PhoneNumber,
-                        Address = comm.Address,
-                        SubCommittees = comm.SubCommittees?.Select(scomm => new SubCommittees
+                return
+                    result.Select(comm =>
+                        new Committee
                         {
-                            ID = comm.ID + scomm.ID,
-                            Address = scomm.Address,
-                            Phone = scomm.PhoneNumber,
-                            Name = scomm.Name,
-                        }).ToHashSet()
-                    }
-                ).ToDictionary(comm => comm.ID);
+                            ID = comm.ID,
+                            CongressType = CongressType.Parse(comm.CongressType),
+                            Name = comm.Name,
+                            Website = comm.Website,
+                            MinorityWebsite = comm.MinorityWebsite,
+                            Jurisdiction = comm.Jurisdiction,
+                            PhoneNumber = comm.PhoneNumber,
+                            Address = comm.Address,
+                            SubCommittees = comm.SubCommittees?.Select(scomm => new SubCommittees
+                            {
+                                ID = comm.ID + scomm.ID,
+                                Address = scomm.Address,
+                                Phone = scomm.PhoneNumber,
+                                Name = scomm.Name,
+                            }).ToHashSet()
+                        }
+                    ).ToDictionary(comm => comm.ID);
+            });
         }
 
 
@@ -223,5 +238,26 @@ namespace GovInformer.Application.Congress
                 }
             }
         }
+
+        private CacheItemPolicy GenerateCachePolicy()
+        {
+            return new CacheItemPolicy() { AbsoluteExpiration = DateTime.Now.AddMinutes(MinutesToLive) };
+        }
+
+        private async Task<T> Gather<T>(string cacheName, Func<Task<T>> generateObject)
+        {
+            if (MemoryCache.Default[cacheName] is T result)
+            {
+                return result;
+            }
+
+            result = await generateObject();
+
+            MemoryCache.Default.Set(cacheName, result, GenerateCachePolicy());
+            return result;
+        }
+
+
+        private readonly int MinutesToLive = 5;
     }
 }
